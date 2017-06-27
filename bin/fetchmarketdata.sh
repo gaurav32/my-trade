6b3495c93721677d9c3177d1bc1b45c885b29f66
sharmagaurav3212130859 <Gyanbharti_04>
2017-06-27 16:37:35 +0530

    datafetchscript

---
+++ bin/fetchmarketdata.sh
@@ -0,0 +1,7 @@
+cd ../src/1_input_marketdatafetcher
+echo "################################## Fetching Market Data..Please wait ###########################"
+python marketdatafetcherdaily19sep.py
+cd ../../bin
+cd ../src/3_processer/datapreparation
+python convert_raw_to_redis_keys_19Sep.py
+cd ../../../bin
