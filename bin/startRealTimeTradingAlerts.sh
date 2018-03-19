94061a524cac8012503c8b65614299af07d3f9af
sharmagaurav3212130859 <Gyanbharti_04>
2018-03-19 23:23:45 +0530

    startup script for reatimetradin alerts

---
+++ bin/startRealTimeTradingAlerts.sh
@@ -0,0 +1,4 @@
+cd ../src/4_output_realtrading/python
+echo "################################## Starting Application ###########################"
+python marketdatastreamingservice.py
+cd ../../../bin
